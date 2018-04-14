ba6e184c860a4561bc1a7b8676a15d1418fd14c5
Dane Tuso <dane@tuso.tech>
2018-04-14 01:48:27 -0700

    typo fix

--- install_script.sh
+++ install_script.sh
@@ -20,7 +20,7 @@ apt-get remove ufw --purge
 #install yarn
 curl -sS https://dl.yarnpkg.com/debian/pubkey.gpg | apt-key add -
 echo "deb https://dl.yarnpkg.com/debian/ stable main" | tee /etc/apt/sources.list.d/yarn.list
-apt-get update && apt-get install yarn
+apt-get update && apt-get install -y yarn
 
 #Install NodeJS
 apt-get isntall -y nodejs npm
