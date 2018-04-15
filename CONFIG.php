19d5de5c54a73ef8450d243235bde8979bf4eadd
Dane Tuso <dane@tuso.tech>
2018-04-15 04:45:40 -0700

    consolidate project to only PHP library with no provisioning

--- CONFIG.php
+++ CONFIG.php
@@ -1,16 +1,22 @@
 <?php
 include_once './includes.php';
+#Allows script to run for as long as needed
 set_time_limit(0);
 ini_set('max_execution_time', 0);
+#Toggle to show debug output log
 define('DEBUG_MODE', TRUE);
-define('VALID_ARGUMENTS', array('help','list','create','destroy'));
+#Digital Ocean API URL
 define('API_URL', 'https://api.digitalocean.com/v2/droplets');
+#Usually around 30 seconds, amount of time before running any provisioning
 define('BOOT_TIME', 40);
+#Used for quickly validating syntax locally
+define('VALID_ARGUMENTS', array('help','list','create','destroy'));
 define('REGIONS', array("nyc1","ams1","sfo1","nyc2","ams2","sgp1","lon1","nyc3","ams3","fra1","tor1"));
 define('SIZES', array("s-1vcpu-1gb","s-1vcpu-2gb","s-1vcpu-3gb","s-2vcpu-2gb","s-3vcpu-1gb","s-2vcpu-4gb","s-4vcpu-8gb","s-6vcpu-16gb","s-8vcpu-32gb","s-12vcpu-48gb","s-16vcpu-64gb","s-20vcpu-96gb","s-24vcpu-128gb","s-32vcpu-192gb"));
-//Change These
+
+//CHANGE THESE
 define('API_TOKEN', '');
-define('SSH_KEY_ID', array());
+define('SSH_KEY_ID', array(00000000));
 define('KEY_PATH', './res/id_rsa');
 define('ANSIBLE_DIR', './Ansible');
 ?>
